2180de0a184673517266a0150c101cd6b0405cc0
Alexis Lopez Zubieta <contact@azubieta.net>
2019-06-13 18:21:14 -0500

    Install missing dependency

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
                ("https://api.bintray.com/conan/conan-community/conan", "yes", "conan-community"),
                ("https://api.bintray.com/conan/appimage-conan-community/public-conan", "yes", "appimage")]
 
-    builder = ConanMultiPackager(build_policy="outdated", remotes=remotes)
-    builder.add_common_builds(shared_option_name="cairo:shared")
+    command = "sudo apt-get -qq update && sudo apt-get -qq install -y gtk-doc-tools"
+    builder = ConanMultiPackager(build_policy="outdated", remotes=remotes, docker_entry_script=command)
+    builder.add_common_builds(shared_option_name="pango:shared")
     builder.run()
